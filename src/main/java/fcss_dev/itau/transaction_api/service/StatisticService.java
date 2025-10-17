b8efba6d9a8475364dba25c2ec0a97f2527438b0
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-10-17 20:30:13 -0300

    Logs
    informa o que está acontecendo enquanto ela trabalha

--- src/main/java/fcss_dev/itau/transaction_api/service/StatisticService.java
+++ src/main/java/fcss_dev/itau/transaction_api/service/StatisticService.java
@@ -4,6 +4,7 @@ package fcss_dev.itau.transaction_api.service;
 import fcss_dev.itau.transaction_api.controller.dto.StatisticResponseDTO;
 import fcss_dev.itau.transaction_api.controller.dto.TransactionRequestDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.DoubleSummaryStatistics;
@@ -11,11 +12,13 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class StatisticService {
 
     public final TransactionService transactionService;
 
     public StatisticResponseDTO calculateStatisticTransaction(Integer intervaloBusca){
+        log.info("Iniciada busca de estatísticas de transações pelo período de tempo" + intervaloBusca);
         List<TransactionRequestDTO> transactions = transactionService.searchTransaction(intervaloBusca);
 
         if (transactions.isEmpty()){
@@ -25,6 +28,7 @@ public class StatisticService {
         DoubleSummaryStatistics StatisticsTransactions = transactions.stream()
                 .mapToDouble(TransactionRequestDTO::valor).summaryStatistics();
 
+        log.info("Estatísticas retornadas com sucesso");
         return new StatisticResponseDTO(StatisticsTransactions.getCount(),
                 StatisticsTransactions.getSum(),
                 StatisticsTransactions.getAverage(),
--- src/main/java/fcss_dev/itau/transaction_api/service/TransactionService.java
+++ src/main/java/fcss_dev/itau/transaction_api/service/TransactionService.java
@@ -3,6 +3,7 @@ package fcss_dev.itau.transaction_api.service;
 import fcss_dev.itau.transaction_api.controller.dto.TransactionRequestDTO;
 import fcss_dev.itau.transaction_api.exceptions.UnprocessableEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
@@ -11,25 +12,36 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TransactionService {
     private final List<TransactionRequestDTO> listTransaction = new ArrayList<>();
 
     public void addTransaction( TransactionRequestDTO dto){
+        log.info("Iniciado o processamento de gravar transações!" + dto);
         if (dto.dataHora().isAfter(OffsetDateTime.now())){
+            log.error("Data e hora maiores que a data atual");
             throw new UnprocessableEntity("");
         }
         if (dto.valor() < 0){
+            log.error("Valor não pode ser menor que 0!");
             throw new UnprocessableEntity("");
         }
+
         listTransaction.add(dto);
+        log.info("Transações adicionadas com sucesso");
     }
 
     public void deleteTransaction(){
+        log.info("Iniciado o processamento para deletar transações");
         listTransaction.clear();
+        log.info("Transações deletadas com sucesso");
     }
 
     public List<TransactionRequestDTO> searchTransaction(Integer intervaloBusca){
+        log.info("Iniciada buscas de Transações por tempo " + intervaloBusca);
         OffsetDateTime dataHoraIntervalo = OffsetDateTime.now().minusSeconds(intervaloBusca);
+
+        log.info("Retorno de transações com sucesso");
         return listTransaction.stream()
                 .filter(t -> t.dataHora()
                         .isAfter(dataHoraIntervalo))
