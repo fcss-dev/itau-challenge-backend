b4a340e0e7284314ea111c33a95daab33c6b0f66
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-10-17 20:56:06 -0300

    Swagger
    documentar como outra pessoa que pegou sua aplicação pela primeira vez pode construir e executar

--- src/main/java/fcss_dev/itau/transaction_api/controller/StatisticsController.java
+++ src/main/java/fcss_dev/itau/transaction_api/controller/StatisticsController.java
@@ -2,6 +2,9 @@ package fcss_dev.itau.transaction_api.controller;
 
 import fcss_dev.itau.transaction_api.controller.dto.StatisticResponseDTO;
 import fcss_dev.itau.transaction_api.service.StatisticService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +20,12 @@ public class StatisticsController {
     private final StatisticService statisticService;
 
     @GetMapping
+    @Operation(description = "Endpoint responsável por buscar estatísticas de transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca efetuada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na busca de estatísticas de transações"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<StatisticResponseDTO> searchStatistics(
             @RequestParam(value = "intervaloBusca", required = false, defaultValue = "60") Integer intervaloBusca){
         return ResponseEntity.ok(statisticService.calculateStatisticTransaction(intervaloBusca));
--- src/main/java/fcss_dev/itau/transaction_api/controller/TransactionController.java
+++ src/main/java/fcss_dev/itau/transaction_api/controller/TransactionController.java
@@ -2,6 +2,9 @@ package fcss_dev.itau.transaction_api.controller;
 
 import fcss_dev.itau.transaction_api.controller.dto.TransactionRequestDTO;
 import fcss_dev.itau.transaction_api.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +16,27 @@ import org.springframework.web.bind.annotation.*;
 public class TransactionController {
     private final TransactionService transactionService;
 
+
     @PostMapping
+    @Operation(description = "Endpoint responsável por adicionar transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação gravada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Campos não atendem os requisitos da transação"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> addTransaction(@RequestBody TransactionRequestDTO dto){
         transactionService.addTransaction(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping
+    @Operation(description = "Endpoint responsável por deletar transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação deletadas com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> deleteTransaction(){
         transactionService.deleteTransaction();
         return ResponseEntity.ok().build();
