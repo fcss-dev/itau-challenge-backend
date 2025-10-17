0a0134101982a6dc1c2b1678790b359ad04cbc5d
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-10-17 13:09:38 -0300

    Initial commit

---
+++ src/test/java/fcss_dev/itau/transaction_api/TransactionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fcss_dev.itau.transaction_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
